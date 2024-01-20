b83bf5fd5f227e7d88a8d2d5321d588b770e50f8
Michal Rynowiecki <michal_rynowiecki@outlook.com>
2024-01-20 19:14:06 +0100

    Introduced player movement (up and down) and started working on enemy movement

--- Pong/MainWindow.xaml.cs
+++ Pong/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace Pong
 {
     /// <summary>
@@ -16,9 +18,70 @@ namespace Pong
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool goUp, goDown;
+        int playerSpeed = 4;
+        int speed = 1;
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+
+            background.Focus();
+
+            gameTimer.Tick += GameTimerEvent;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Start();
+
+        }
+
+        //Movement
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            //Player Movement
+            if (goDown==true && Canvas.GetBottom(playerBox) > 5)
+            {
+                Canvas.SetBottom(playerBox, Canvas.GetBottom(playerBox) - playerSpeed);
+            }
+
+            
+            if (goUp == true && Canvas.GetBottom(playerBox) < 350)
+            {
+                Canvas.SetBottom(playerBox, Canvas.GetBottom(playerBox) + playerSpeed);
+            }
+
+            //Enemy movement
+            if (Canvas.GetBottom(enemyBox) > Canvas.GetBottom(playerBox) && Canvas.GetBottom(enemyBox) > 5)
+            {
+                Canvas.SetBottom(enemyBox, Canvas.GetBottom(playerBox) - speed);
+            }
+
+                //Canvas.SetBottom(enemyBox, Canvas.GetBottom(playerBox) + speed);
+            
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Down)
+            {
+                goDown = true;
+            }
+            if (e.Key == Key.Up) 
+            {
+                goUp = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Down)
+            {
+                goDown = false;
+            }
+            if (e.Key == Key.Up)
+            {
+                goUp = false;
+            }
         }
     }
 }
